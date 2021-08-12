f9c6f8fd77ebb865d6a22136b59a18dd310893e7
Leslie <leslie_10345@126.com>
2021-08-12 10:04:51 +0800

    diyicijilu

---
+++ webris-base/src/test/java/com/tj/webris/WebrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.webris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
